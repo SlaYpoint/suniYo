3045bd8056b3c1751f04e84023968a42a895cf8c
adiMallya <adimallya99@gmail.com>
2021-05-25 18:20:06 +0530

    fix : async calls for trackHandler

--- src/App.js
+++ src/App.js
@@ -7,36 +7,53 @@ import Player from "./components/Player/Player";
 
 function App() {
   const audioRef = useRef(null);
-
+  //eslint-disable-next-line
   const [tracks, setTracks] = useState(data);
   const [currentTrack, setCurrentTrack] = useState(tracks[0]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackInfo, setTrackInfo] = useState({
     currentTime: 0,
-    duration: 0
+    duration: 0,
   });
 
   const timeUpdateHandler = (e) => {
     const currentTime = e.target.currentTime;
     const duration = e.target.duration;
-    setTrackInfo(...trackInfo, currentTime, duration);
-  }
+    setTrackInfo({ ...trackInfo, currentTime, duration });
+  };
 
-  const trackHandler = () => {
-    const currentIndex = tracks.findIndex((track) => track.id === currentTrack.id);
+  const trackHandler = async () => {
+    const currentIndex = tracks.findIndex(
+      (track) => track.id === currentTrack.id
+    );
     const nextTrack = tracks[(currentIndex + 1) % tracks.length];
-    setCurrentTrack(nextTrack);
+    await setCurrentTrack(nextTrack);
 
     if (isPlaying) {
       audioRef.current.play();
     }
-  }
+  };
 
   return (
     <div className="App">
       <h1>suniYo</h1>
-      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentTrack={currentTrack} setCurrentTrack={setCurrentTrack} audioRef={audioRef} trackInfo={trackInfo} setTrackInfo={setTrackInfo}/>
-      <audio onEnded={trackHandler} onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentTrack.audio}></audio>
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentTrack={currentTrack}
+        setCurrentTrack={setCurrentTrack}
+        audioRef={audioRef}
+        trackInfo={trackInfo}
+        setTrackInfo={setTrackInfo}
+        tracks={tracks}
+      />
+      <audio
+        onLoadedMetaData={timeUpdateHandler}
+        onEnded={trackHandler}
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentTrack.audio}
+      />
     </div>
   );
 }
