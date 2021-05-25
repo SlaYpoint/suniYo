8db50915ead9fcd5a6c3abfaa9f4c60c00be4baf
adiMallya <adimallya99@gmail.com>
2021-05-25 17:12:13 +0530

    added drag handler

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -7,6 +7,7 @@ const Player = ({
     isPlaying,
     setIsPlaying,
     currentTrack,
+    //eslint-disable-next-line
     setCurrentTrack,
     audioRef,
     trackInfo,
@@ -23,18 +24,32 @@ const Player = ({
         }
     }
 
+    const getTime = (time) => {
+      let min = Math.floor(time / 60);
+      let sec = ("0" + Math.floor(time % 60)).slice(-2);
+      return `${min}:${sec}`;
+    }
+  
+    const dragHandler = (e) =>{
+      audioRef.current.currentTime = e.target.value;
+      setTrackInfo({ ...trackInfo, currentTime: e.target.value });
+    }
+
     return (
       <div className="player__container">
         {/* <div className="audio__img-container">
                 <img src="" alt="" className="audio__img"/>
             </div> */}
         <div className="track__progress">
-          <p>0:00</p>
-          <div className="track">
-            <input type="range" name="" id="" />
-            <div className="track__progress-bar"></div>
+          <p>{getTime(trackInfo.currentTime || 0)}</p>
+          <div
+            className="track"
+            style={{background : `linear-gradient(to right, ${currentTrack.color[0]}, ${currentTrack.color[1]})`}}
+          >
+            <input type="range" onChange={dragHandler} min={0} max={trackInfo.duration} value={trackInfo.currentTime}/>
+            <div className="track__progress-bar" style={{transform : `translateX(${Math.round((trackInfo.currentTime*100)/ trackInfo.duration)+"%"})`}}></div>
           </div>
-          <p>2:13</p>
+          <p>{getTime(trackInfo.duration || 0)}</p>
         </div>
 
         <div class="player__controls">
