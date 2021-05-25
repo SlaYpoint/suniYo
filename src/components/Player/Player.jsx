ea40b1ea91ddf83cf5d73dd7e843cb0f7778e72e
adiMallya <adimallya99@gmail.com>
2021-05-26 00:02:56 +0530

    style : added animations

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -54,48 +54,62 @@ const Player = ({
     
     return (
       <div className="player__container">
-        {/* <div className="audio__img-container">
-                <img src="" alt="" className="audio__img"/>
-            </div> */}
-        <div className="track__progress">
-          <p>{getTime(trackInfo.currentTime || 0)}</p>
-          <div
-            className="track"
-            style={{
-              background: `linear-gradient(to right, ${currentTrack.color[0]}, ${currentTrack.color[1]})`,
-            }}
-          >
-            <input
-              type="range"
-              onChange={dragHandler}
-              min={0}
-              max={trackInfo.duration}
-              value={trackInfo.currentTime}
-            />
-            <div
-              className="track__progress-bar"
-              style={{
-                transform: `translateX(${
-                  Math.round(
-                    (trackInfo.currentTime * 100) / trackInfo.duration
-                  ) + "%"
-                })`,
-              }}
-            ></div>
-          </div>
-          <p>{getTime(trackInfo.duration || 0)}</p>
+        <div className="track__img-container">
+          <img
+            src={currentTrack.cover}
+            alt={currentTrack.name}
+            className="track-img"
+            style={{ animationPlayState: isPlaying ? 'running' : 'paused'}}
+          /> 
         </div>
 
-        <div class="player__controls">
-          <button onClick={() => skipHandler("backward")}>
-            <FaAngleLeft />
-          </button>
-          <button onClick={playHandler}>
-            {isPlaying ? <FaPause /> : <FaPlay />}
-          </button>
-          <button onClick={() => skipHandler("forward")}>
-            <FaAngleRight />
-          </button>
+        <div className="track__container">
+          <div className="track__info">
+            <div className="track__title">{currentTrack.name}</div>
+            <div className="track__artist">{currentTrack.artist}</div>
+          </div>
+          <div className="player">
+            <div className="player__track">
+              <p>{getTime(trackInfo.currentTime || 0)}</p>
+              <div
+                className="audio"
+                style={{
+                  background: `linear-gradient(to right, ${currentTrack.color[0]}, ${currentTrack.color[1]})`,
+                }}
+              >
+                <input
+                  type="range"
+                  onChange={dragHandler}
+                  min={0}
+                  max={trackInfo.duration}
+                  value={trackInfo.currentTime}
+                />
+                <div
+                  className="audio__progress-bar"
+                  style={{
+                    transform: `translateX(${
+                      Math.round(
+                        (trackInfo.currentTime * 100) / trackInfo.duration
+                      ) + "%"
+                    })`,
+                  }}
+                ></div>
+              </div>
+              <p>{getTime(trackInfo.duration || 0)}</p>
+            </div>
+
+            <div class="player__controls">
+              <button onClick={() => skipHandler("backward")}>
+                <FaAngleLeft />
+              </button>
+              <button onClick={playHandler}>
+                {isPlaying ? <FaPause /> : <FaPlay />}
+              </button>
+              <button onClick={() => skipHandler("forward")}>
+                <FaAngleRight />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
