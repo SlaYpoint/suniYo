ad750fcf65eb31e016cd006f7e02ac4e5b591859
SlaYpoint <adimallya99@gmail.com>
2021-06-06 00:48:38 +0530

    fetched data from deezerAPI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-//eslint-disable-next-line
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
-//eslint-disable-next-line
-// import data from "./data";
+
 import Player from "./components/Player/Player";
 import Header from "./components/Header/Header";
 
@@ -12,44 +10,55 @@ import { getPlaylist } from './utils/makeAxiosCalls';
 function App() {
   const audioRef = useRef(null);
   //eslint-disable-next-line
-  
+  const [loading, setLoading] = useState(false);
   const [tracks, setTracks] = useState([]);
-  const [currentTrack, setCurrentTrack] = useState(tracks[0]);
+  const [currentTrack, setCurrentTrack] = useState({});
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackInfo, setTrackInfo] = useState({
     currentTime: 0,
     duration: 0,
   });
 
-  
+  const fetchDefaultTracks = () => {
+    setLoading(true);
+    try {
+      const newTracks = getPlaylist();
+      newTracks.then((data) => setTracks(data));
+      setLoading(false);
+      // console.log(tracks);
+      setCurrentTrack(tracks[0]);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  }
+
   useEffect(() => {
-    const newTracks = getPlaylist();
-    setTracks(newTracks);
+    fetchDefaultTracks();
   }, []);
 
-    console.log(tracks);
-  // const timeUpdateHandler = (e) => {
-  //   const currentTime = e.target.currentTime;
-  //   const duration = e.target.duration;
-  //   setTrackInfo({ ...trackInfo, currentTime, duration });
-  // };
+  const timeUpdateHandler = (e) => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    setTrackInfo({ ...trackInfo, currentTime, duration });
+  };
 
-  // const trackHandler = async () => {
-  //   const currentIndex = tracks.findIndex(
-  //     (track) => track.id === currentTrack.id
-  //   );
-  //   const nextTrack = tracks[(currentIndex + 1) % tracks.length];
-  //   await setCurrentTrack(nextTrack);
+  const trackHandler = async () => {
+    const currentIndex = tracks.findIndex(
+      (track) => track.id === currentTrack.id
+    );
+    const nextTrack = tracks[(currentIndex + 1) % tracks.length];
+    await setCurrentTrack(nextTrack);
 
-  //   if (isPlaying) {
-  //     audioRef.current.play();
-  //   }
-  // };
+    if (isPlaying) {
+      audioRef.current.play();
+    }
+  };
 
   return (
     <div className="App">
-      <Header/>
-      {/* <Player
+      <Header />
+      <Player
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
         currentTrack={currentTrack}
@@ -58,14 +67,14 @@ function App() {
         trackInfo={trackInfo}
         setTrackInfo={setTrackInfo}
         tracks={tracks}
-      /> */}
-      {/* <audio
+      />
+      <audio
         onLoadedMetadata={timeUpdateHandler}
         onEnded={trackHandler}
         onTimeUpdate={timeUpdateHandler}
         ref={audioRef}
-        src={currentTrack.audio}
-      /> */}
+        src={currentTrack.preview}
+      />
     </div>
   );
 }
