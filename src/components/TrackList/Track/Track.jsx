a177abccb1eeb23773f3a7b4d1fd206294dd9fa9
SlaYpoint <codersaysady@gmail.com>
2021-06-07 19:45:58 +0530

    added track component

--- src/components/TrackList/Track/Track.jsx
+++ src/components/TrackList/Track/Track.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./Track.css";
 
-const Track = ({ track, setCurrentTrack, isPlaying, tracks, setTracks, audioRef}) => {
+const Track = ({ track, setCurrentTrack, isPlaying, audioRef}) => {
+    
+    const trackSelectHandler = () => {
+        setCurrentTrack(track);
+
+        if (isPlaying) {
+            audioRef.current.play();
+        }
+    }
+
     return (
-        <li key={track.id}>
+        <li key={track.id} onClick={trackSelectHandler}>
             <figure className="track__cover">
                 <img src={track.album.cover_small} alt={track.title_short}/>
             </figure>
             <div className="track__details">
                 <h4 className="track__title">{track.title_short}</h4>
-                <p className="track_artist">{track.artist.name}</p>
+                <p className="track__artist">{track.artist.name}</p>
             </div>
         </li>
     );
