ebe1cbe42d25c8b7f5d26e5fb0089d90cd155cae
SlaYpoint <codersaysady@gmail.com>
2021-06-07 19:46:13 +0530

    added trackList

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import Pagination from "./Pagination/Pagination";
 import Track from "./Track/Track";
 
-const TrackList = ({tracks, setTracks, currentTrack, setCurrentTrack, audioRef, isPlaying}) => {
+const TrackList = ({tracks, setTracks, currentTrack, setCurrentTrack, audioRef, isPlaying, loading}) => {
+    
+    const [currentPage, setCurrentPage] = useState(1);
+    const [tracksPerPage] = useState(5);
+
+    const endIdx = currentPage * tracksPerPage;
+    const startIdx = endIdx - tracksPerPage;
+
+    const paginate = (pageNum) => {
+        setCurrentPage(pageNum);
+    }
+
     return (
         <div className="tracks__container">
             <ul className="track__list">
-                {tracks.map((track) => (
+                {
+                    tracks.slice(startIdx,endIdx).map((track) => (
                     <Track
                         track={track}
                         setCurrentTrack={setCurrentTrack}
                         isPlaying={isPlaying}
-                        tracks={tracks}
-                        setTracks={setTracks}
                         audioRef={audioRef}
                     />
-                ))}
+                    ))
+                }
             </ul>
-            <Pagination/>
+            <Pagination
+                page={currentPage}
+                postsPerPage={tracksPerPage}
+                totalPosts={tracks.length}
+                paginate={paginate}
+            />
         </div>
     );
 }
