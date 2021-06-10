9d9d38decda8daa58a01b009f10e21ad5ef1da93
SlaYpoint <codersaysady@gmail.com>
2021-06-11 00:46:02 +0530

    fix : fixed title lengths

--- src/components/TrackList/Track/Track.jsx
+++ src/components/TrackList/Track/Track.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import limitTitle from "../../../helpers/limitTitle";
 
 const Track = ({ track, setCurrentTrack, isPlaying, audioRef}) => {
     
@@ -13,10 +14,10 @@ const Track = ({ track, setCurrentTrack, isPlaying, audioRef}) => {
     return (
         <li className="track" key={track.id} onClick={trackSelectHandler}>
             <figure className="track__cover">
-                <img src={track.album.cover_small} alt={track.title_short}/>
+                <img src={track.album.cover_small} alt={limitTitle(track.title_short)}/>
             </figure>
             <div className="track__details">
-                <h4 className="track__title">{track.title_short}</h4>
+                <h4 className="track__title">{limitTitle(track.title_short)}</h4>
                 <p className="track__artist">{track.artist.name}</p>
             </div>
         </li>
