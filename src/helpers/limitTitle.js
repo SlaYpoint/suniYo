d3f4784338572038a56bf9464a0fac89bbf2c685
SlaYpoint <codersaysady@gmail.com>
2021-06-11 00:45:21 +0530

    added limitTitle function

---
+++ src/helpers/limitTitle.js
@@ -0,0 +1,16 @@
+const limitTitle = (title, limit = 15) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, curr) => {
+            if (acc + curr.length <= limit) {
+                newTitle.push(curr);
+            }
+            return acc + curr.length;
+        }, 0);
+
+        return `${newTitle.join(' ')}...`;
+    }
+    return title;
+}
+
+export default limitTitle;
