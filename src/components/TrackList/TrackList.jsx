6bd907e14bece514919af72a8f75fa0bcc273b39
SlaYpoint <codersaysady@gmail.com>
2021-06-11 00:46:58 +0530

    added pagination

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,41 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import "./TrackList.css";
 import Pagination from "./Pagination/Pagination";
 import Track from "./Track/Track";
 
-const TrackList = ({tracks, setTracks, currentTrack, setCurrentTrack, audioRef, isPlaying, loading}) => {
-    
-    const [currentPage, setCurrentPage] = useState(1);
-    const [tracksPerPage] = useState(5);
+const TrackList = ({tracks, setCurrentTrack, audioRef, isPlaying}) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [tracksPerPage] = useState(5);
 
-    const endIdx = currentPage * tracksPerPage;
-    const startIdx = endIdx - tracksPerPage;
+  const endIdx = currentPage * tracksPerPage;
+  const startIdx = endIdx - tracksPerPage;
+  const paginate = (pageNum) => {
+    setCurrentPage(pageNum);
+  };
 
-    const paginate = (pageNum) => {
-        setCurrentPage(pageNum);
-    }
-
-    return (
-        <div className="tracks__container">
-            <ul className="track__list">
-                {
-                    tracks.slice(startIdx,endIdx).map((track) => (
-                    <Track
-                        track={track}
-                        setCurrentTrack={setCurrentTrack}
-                        isPlaying={isPlaying}
-                        audioRef={audioRef}
-                    />
-                    ))
-                }
-            </ul>
-            <Pagination
-                page={currentPage}
-                postsPerPage={tracksPerPage}
-                totalPosts={tracks.length}
-                paginate={paginate}
-            />
-        </div>
-    );
+  return (
+    <div className="track__container">
+      <ul className="track__list">
+        {tracks.slice(startIdx, endIdx).map((track) => (
+          <Track
+            track={track}
+            setCurrentTrack={setCurrentTrack}
+            isPlaying={isPlaying}
+            audioRef={audioRef}
+          />
+        ))}
+      </ul>
+      <Pagination
+          page={currentPage}
+          postsPerPage={tracksPerPage}
+          totalPosts={tracks.length}
+          paginate={paginate}
+      />
+    </div>
+  );
 }
 
 export default TrackList;
