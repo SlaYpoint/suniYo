598766241973ef76ba5b8e193e44a5c58580ce44
SlaYpoint <codersaysady@gmail.com>
2021-07-20 01:21:03 +0530

    created global context

---
+++ src/contexts/globalContext.js
@@ -0,0 +1,42 @@
+import React, { useState, useRef, useContext } from 'react';
+import data from "../helpers/data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+
+    const audioRef = useRef(null);
+
+    const [tracks, setTracks] = useState([]);
+    const [currentTrack, setCurrentTrack] = useState(data[0]);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const playSong = () => setIsPlaying(true);
+
+    const pauseSong = () => setIsPlaying(false);
+
+
+    return (
+        <AppContext.Provider
+            value={{
+                tracks,
+                setTracks,
+                currentTrack,
+                setCurrentTrack,
+                isPlaying,
+                playSong,
+                pauseSong,
+                audioRef
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+} 
+
+export { AppContext, AppProvider }
+
